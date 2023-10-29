c3043b1f21b2b58ac74e63fbbfb45a7e8648d4ba
raviehasan <ravie.hasan@ui.ac.id>
2023-10-29 15:13:07 +0700

    add books/ and update leaderboard/

--- main/views.py
+++ main/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
+from book.models import Book
+from django.core import serializers
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+@csrf_exempt
 def show_main(request):
-
+    books = Book.objects.all()
     context = {
-        'test':'testing'
+        'books':books,
     }
-
     return render(request, "main.html", context)
+
+@csrf_exempt
+def show_books(request):
+    books = Book.objects.all()
+    context = {
+        'books':books,
+    }
+    return render(request, "books.html", context)
+
+@csrf_exempt
+def get_books(request):
+    books = Book.objects.all()
+    return HttpResponse(serializers.serialize("json", books), content_type="application/json")
