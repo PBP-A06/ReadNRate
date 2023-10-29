4757af6a5a8e09784101993063da1245de172b62
ArditheusIH <arditheus.immanuel@gmail.com>
2023-10-30 01:29:18 +0700

    merge from ravie2

--- main/models.py
+++ main/models.py
@@ -5,11 +5,3 @@ class User(models.Model):
     username = models.CharField(max_length=255)
     name = models.CharField(max_length=255)
 
-class Readlist(models.Model):
-    title = models.CharField(max_length=100)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    description = models.TextField()
-    books = models.ManyToManyField(Book, blank=True)
- 
-    def __str__(self):
-        return self.title
