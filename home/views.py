0bbf0581afaf6bec8e427970445fa9157ef2ba3c
raviehasan <ravie.hasan@ui.ac.id>
2023-10-29 10:34:21 +0700

    tailwind n home

--- home/views.py
+++ home/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def show_home(request):
-    return render(request, "home.html")
+    context = {}
+    return render(request, "home.html", context)
