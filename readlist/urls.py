ff3311edf9a8476f90a5df3514729375cff36803
ArditheusIH <arditheus.immanuel@gmail.com>
2023-10-30 20:17:30 +0700

    connected readlist to detail-readlist.html

--- readlist/urls.py
+++ readlist/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from readlist.views import *
+from leaderboard.views import show_readlist_by_id
 
 app_name = 'readlist'
 
 urlpatterns = [
     path('', show_readlist, name='show_readlist'),
-    path('create_readlist/', create_readlist, name='create_readlist'),
-    path('readlist_detail/<int:readlist_id>', readlist_detail, name='readlist_detail'),
+    path('create-readlist/', create_readlist, name='create_readlist'),
+    path("readlist-id-<int:pk>/", show_readlist_by_id, name="show_readlist_by_id"),
+    path("get-readlists/", get_readlists, name="get_readlists"),
+    path("get-books/", get_books, name="get_books"),
+    #  path('redirect/', redirect_to_other_app, name='redirect'),
+    # path('readlist_detail/<int:readlist_id>', readlist_detail, name='readlist_detail'),
 ]
--- readlist/views.py
+++ readlist/views.py
@@ -6,12 +6,13 @@ from .models import Readlist, Book
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
 #show all existing readlists
 def show_readlist(request):
-    top_5_readlists = Readlist.objects.all()
+    top_5_readlists = Readlist.objects.all()[0:5]
 
     context = {
         'top_5_readlists': top_5_readlists,
@@ -22,11 +23,11 @@ def show_readlist(request):
 #get readlist details
 def readlist_detail(request, readlist_id):
     readlist = get_object_or_404(Readlist, id=readlist_id)
-    return HttpResponse(serializers.serialize("json", readlist), content_type="application/json")
-    # return render(request, 'readlist_detail.html', {'readlist': readlist})
+    # return HttpResponse(serializers.serialize("json", readlist), content_type="application/json")
+    return render(request, 'readlist_detail.html', {'readlist': readlist})
 
 #TODO: make login required
-# @login_required(login_url='/login')
+@login_required(login_url='/login')
 def create_readlist(request):
     if request.method == 'POST':
         form = AddBooksToReadlistForm(request.POST)
@@ -40,11 +41,6 @@ def create_readlist(request):
         form = AddBooksToReadlistForm()
     return render(request, 'create_readlist.html', {'form': form})
 
-#get all readlist
-def get_readlists():
-    readlists = Readlist.objects.all()
-    return HttpResponse(serializers.serialize('json', readlists))
-
 #TODO:fix likes
 #increment number of likes
 def like_a_readlist(request, id):
@@ -53,4 +49,30 @@ def like_a_readlist(request, id):
     updated_readlist.save()
     return HttpResponseRedirect(reverse('main:show_main'))
 
+@csrf_exempt
+def get_books(request):
+    data = Book.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json") 
 
+@csrf_exempt
+def show_book_by_id(request, pk):
+    book = Book.objects.filter(pk=pk)[0]
+    context = {
+        'book':book,
+    }
+    return render(request, "leaderboard:details-book.html", context)
+
+@csrf_exempt
+def get_readlists(request):
+    readlists = Readlist.objects.all()
+    return HttpResponse(serializers.serialize("json", readlists), content_type="application/json") 
+
+@csrf_exempt
+def show_readlist_by_id(request, pk):
+    readlist = Readlist.objects.filter(pk=pk)[0]
+    books = readlist.books.all()
+    context = {
+        'readlist':readlist,
+        'books':books,
+    }
+    return render(request, "details-readlist.html", context)
