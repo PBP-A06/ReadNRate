dd32ca960f1f38004babddd98d3ef89cd22c0364
sorfeb <soros21febriano@gmail.com>
2023-12-03 09:10:54 +0700

    fixed registration

--- authentication/views.py
+++ authentication/views.py
@@ -1,8 +1,13 @@
+import json
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login as auth_login
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import User
+
+from authentication.forms import RegistrationForm
 
 @csrf_exempt
 def login(request):
@@ -47,3 +52,47 @@ def logout(request):
         "status": False,
         "message": "Logout gagal."
         }, status=401)
+    
+@csrf_exempt
+def register(request):
+    if request.method != "POST":
+        return JsonResponse({
+            "status": "failed",
+            "message": "Register gagal. Bukan method POST"
+            }, status=401)   
+
+    if username == "" or password == "":
+            return JsonResponse({
+            "status": "failed",
+            "message": "Register gagal. Ada fields kosong."
+            }, status=401)
+    
+    if password != password_confirmation:
+            return JsonResponse({
+            "status": "failed",
+            "message": "Register gagal. Password tidak match."
+            }, status=401)   
+
+    data = json.loads(request.body)
+    username = data['username']
+    password = data['password']
+    password_confirmation = data['passwordConfirmation']
+    
+    try: 
+        user = User.objects.create(
+            username = username,
+            password = make_password(password),
+        )
+        user.save
+
+        return JsonResponse({
+            'status': 'success',
+            'message': 'Berhasil register',
+            }, status=201)
+    
+    except Exception:
+        return JsonResponse({
+            "status": "failed",
+            "message": f"Register gagal. Sudah ada pengguna dengan username {username}."
+            }, status=401)
+    
