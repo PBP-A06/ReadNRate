1e09049c2963a0fc714149aceedffb9b038cff3e
Daffa <daffaalfathanzaki@gmail.com>
2023-12-04 17:59:09 +0700

    Revisi profile

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -4,9 +4,5 @@ from user_profile.views import *
 app_name = 'user_profile'
 
 urlpatterns = [
-    # path('', ),
-    path("show-bookmarked/", show_bookmarked, name="show_bookmarked"),
-    path("show-liked/", show_liked, name="show_liked"),
     path("", show_profile, name="show_profile"),
-    # path('show-profile/', show_profile, name='show_profile'),
 ]
