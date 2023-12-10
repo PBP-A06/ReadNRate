843590c15124d7af4e785c7c7d0de2b7936fff66
ffatihzain <fatihzain10@gmail.com>
2023-12-10 15:31:43 +0700

    fix bookReview_review db error

--- bookReview/urls.py
+++ bookReview/urls.py
@@ -5,7 +5,7 @@ app_name = "bookReview"
 
 urlpatterns = [
     path('book/<int:id>/', show_book_detail, name='book_detail'),
-    path('add_review/<int:book_id>/', add_review_ajax, name='add_review'),
+    path('add_review/<int:id>/', add_review_ajax, name='add_review'),
     path('path_to_your_api/', submit_review, name='submit_review'),
     path('toggle_bookmark/<int:id>/', toggle_bookmark, name='toggle_bookmark'),
     path('toggle_like/<int:id>/', toggle_like, name='toggle_like'),
