b95586be21884ed58d868d3a402d57148903c1d3
Daffa <daffaalfathanzaki@gmail.com>
2023-12-11 14:05:50 +0700

    Ubah review

--- user_profile/views.py
+++ user_profile/views.py
@@ -9,7 +9,7 @@ from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='/login')
 def show_profile(request):
-    books_read = Review.objects.filter(user=request.user)
+    books_read = Book.objects.filter(review__user=request.user).distinct()
     bookmarked_books = Book.objects.filter(bookmarked_by=request.user)
     liked_books = Book.objects.filter(liked_by=request.user)
 
@@ -19,3 +19,18 @@ def show_profile(request):
         'liked_books': liked_books,
     }
     return render(request, "profile.html", context)
+
+@login_required(login_url='/login')
+def show_profile_json(request):
+    books_read = Book.objects.filter(review__user=request.user).distinct()
+    bookmarked_books = Book.objects.filter(bookmarked_by=request.user)
+    liked_books = Book.objects.filter(liked_by=request.user)
+
+    # Serialize the data into JSON format
+    serialized_data = {
+        'books_read': list(books_read.values()),  # Serialize queryset to a list of dictionaries
+        'bookmarked_books': list(bookmarked_books.values()),
+        'liked_books': list(liked_books.values()),
+    }
+
+    return JsonResponse(serialized_data, safe=False)  # Return serialized data as JSON response
