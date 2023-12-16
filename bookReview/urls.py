5d357e91aa85b9fd5c15f32f7031e2fec5942155
ffatihzain <119392824+ffatihzain@users.noreply.github.com>
2023-12-16 17:02:50 +0700

    adding get_reviews

--- bookReview/urls.py
+++ bookReview/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('path_to_your_api/', submit_review, name='submit_review'),
     path('toggle_bookmark/<int:id>/', toggle_bookmark, name='toggle_bookmark'),
     path('toggle_like/<int:id>/', toggle_like, name='toggle_like'),
+    path('get_reviews/<int:book_id>/', get_reviews, name='get_reviews'),
 ]
